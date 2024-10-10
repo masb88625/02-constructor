e4bc0488be0ffd059cf6be0b07addcd381f0ff70
maskur <146052741+masb88625@users.noreply.github.com>
2024-10-10 22:25:52 +0800

    Add files via upload

---
+++ 02-constuctor.java
@@ -0,0 +1,35 @@
+//Class polos adalah class tanpa konstruktor
+class Polos{
+    //atribut
+    String datastring;
+    int dataInteger;
+   }
+
+// class dengan constructor
+Class Mahasiswa{
+    // atribut
+    String nama;
+    String NIM;
+    String prodi;
+
+    // constructor
+    // namanya sama dengan nama class
+    // tidak memiliki return value/nilai balik
+    // method yang pertama kali dipanggil saat objek pertama kali dibuat
+   Mahasiswa (String inputNama, String inputNIM, String inputProdi){
+        // mengisi data
+        Nama = inputNama;
+        NIM = InputNIM;
+        Prodi = inputProdi;
+
+        // mencetak data
+        System.out.println("Nama Mahasiswa : " + nama);
+        System.out.println("NIM : " + NIM);
+        System.out.println("Prodi : " + prodi);
+
+    
+
+
+
+
+}
